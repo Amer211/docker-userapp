97c515d1d166f9ab778e8a2c9c172ff0d290efde
Amer211 <mokhbi.ameur@gmail.com>
2025-09-26 15:55:36 -0400

    1. @RequestApi "/users" was added. 2. @Cacheable was added to the function getUserById to use Redis caching functionality.

--- demo/demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -2,12 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.service.UserService;
 import com.example.demo.user.User;
-import jakarta.persistence.Cacheable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private static final Logger logger = LogManager.getLogger(UserController.class);
@@ -32,8 +34,9 @@ public class UserController {
     }
 
 
-    @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id){
+    @GetMapping("/{id}")                    //=> next line to add Redis for caching ..
+    @Cacheable(value = "users", key = "#id") // @cacheable package: org.springframework.
+    public User getUserById(@PathVariable Long id){     // .cache.annotation.Cacheable;
      logger.info("fetching user with id :{}",id);
      return userService.getUserById(id);
     }
